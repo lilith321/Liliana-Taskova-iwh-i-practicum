8907a1b9474604027a0c8d4ecdad9ad170cfe4c6
lilith321 <138465007+lilith321@users.noreply.github.com>
2025-01-17 00:37:20 +0200

    Add index route and view

--- index.js
+++ index.js
@@ -13,6 +13,21 @@ const PRIVATE_APP_ACCESS = '';
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 // * Code for Route 1 goes here
+app.get('/', async (req, res) => {
+    const cakes = 'https://api.hubspot.com/crm/v3/objects/2-138167251/?properties=name&properties=type&properties=size';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json',
+    };
+    try {
+        const resp = await axios.get(cakes, { headers });
+        const data = resp.data.results;
+        console.log(data);
+        res.render('cakes', { title: 'Cakes | HubSpot APIs', data });      
+    } catch (error) {
+        console.error(error);
+}
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
